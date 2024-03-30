420c2a69c6e9ed25b56dd3abc85a31c4e35f691b
essozmdr1 <88781587+essozmdr1@users.noreply.github.com>
2024-03-30 15:08:16 +0300

    login and register pages has been created

--- server.py
+++ server.py
@@ -1,22 +1,29 @@
-# app.py
+from flask import Flask, render_template, redirect, request
 
-from flask import Flask, render_template, request
-from model import find_similar_images
+
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        # Kullanıcı tarafından yüklenen dosya
-        input_image = request.files["input_image"]
-        
-        # Kullanıcı tarafından yüklenen dosyanın benzerlerini bul
-        similar_images = find_similar_images(input_image, dataset_path='C:\\Users\\emiry\\melspectograms')
+@app.route('/')
+def index():
+    return render_template('home_page.html')
 
-        return render_template("result.html", input_image=input_image, similar_images=similar_images)
-    
-    return render_template("home.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Giriş işlemleri burada yapılacak
+        return redirect('/')  # Başarılı giriş durumunda ana sayfaya yönlendir
+    return render_template('login.html')  # login.html dosyasını render et
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        # Kayıt işlemleri burada yapılacak
+        return redirect('/')  # Başarılı kayıt durumunda ana sayfaya yönlendir
+    return render_template('register.html')  # register.html dosyasını render et
+
 
 if __name__ == "__main__":
     app.run(debug=True)
